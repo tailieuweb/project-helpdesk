2d6b62c6377d27fd6d9417937d9201f1bb316951
phanthe <thept@tdc.edu.vn>
2022-03-14 23:58:40 +0700

    abc

--- vendor/foostart/package-acl/resources/views/admin/layouts/base.blade.php
+++ vendor/foostart/package-acl/resources/views/admin/layouts/base.blade.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>@yield('title')</title>
+    <title>@yield('title')HelpDesk</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="keywords" content="">
